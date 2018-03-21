fdbfe7a42719134171e78fe03baa21be6c97a7ae
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-03-21 10:44:22 -0300

    Primeira versão

--- SwipecardsExemplo/SwipecardsExemplo/MainPage.xaml.cs
+++ SwipecardsExemplo/SwipecardsExemplo/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SwipecardsExemplo.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace SwipecardsExemplo
 		public MainPage()
 		{
 			InitializeComponent();
-		}
+            BindingContext = new MainViewModel();
+        }
 	}
 }
---
+++ SwipecardsExemplo/SwipecardsExemplo/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SwipecardsExemplo.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        ObservableCollection<string> cartoes;
+        public ObservableCollection<string> Cartoes
+        {
+            get { return cartoes; }
+            set { cartoes = value; RaisePropertyChanged(); }
+        }
+
+        public MainViewModel()
+        {
+            cartoes = new ObservableCollection<string>
+            {
+                "Nintendo Switch",
+                "Playstation 4",
+                "Xbox One",
+                "PC"
+            };
+        }
+
+        // INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        void RaisePropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
